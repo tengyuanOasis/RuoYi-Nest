6138311cf2ce45095d996fd9233de5004b0a942a
erhu <986171791@qq.com>
2025-05-18 19:51:23 +0800

    修复代码生成控制器注释错误

--- ruoyi-nest/src/ruoyi-share/utils/hbs-controller-render.utils.ts
+++ ruoyi-nest/src/ruoyi-share/utils/hbs-controller-render.utils.ts
@@ -48,13 +48,13 @@ export class ${context.ClassName}Controller extends BaseController {
     }
 
     /**
-     * 查询参数配置表列表
+     * 查询${context.functionName}列表
      */
-    @ApiOperation({ summary: '查询参数配置表列表' })
+    @ApiOperation({ summary: '查询${context.functionName}列表' })
     @ApiResponse({ status: 200, description: '查询成功', type: [${context.ClassName}] })
     @PreAuthorize('${context.permissionPrefix}:list')
     @Get('list')
-    @Log({ title: '查询参数配置表列表' })
+    @Log({ title: '查询${context.functionName}列表' })
     async list(@Query() query: ${context.ClassName}) {
         this.startPage(query);
         const [list, total] = await this.${ClassNameWithoutSysPrefixAndLowerCaseFirstLetter}Service.select${ClassNameWithoutSysPrefix}List(query);
@@ -62,38 +62,38 @@ export class ${context.ClassName}Controller extends BaseController {
     }
 
     /**
-     * 导出参数配置表列表
+     * 导出${context.functionName}列表
      */
-    @ApiOperation({ summary: '导出参数配置表列表' })
+    @ApiOperation({ summary: '导出${context.functionName}列表' })
     @ApiResponse({ status: 200, description: '导出成功' })
     @PreAuthorize('${context.permissionPrefix}:export')
     @Post('export')
-    @Log({ title: '导出参数配置表', businessType: BusinessType.EXPORT })
+    @Log({ title: '导出${context.functionName}', businessType: BusinessType.EXPORT })
     async export(@Res() res, @Body() query: ${context.ClassName}) {
         const [list] = await this.${ClassNameWithoutSysPrefixAndLowerCaseFirstLetter}Service.select${ClassNameWithoutSysPrefix}List(query);
         await this.excelUtils.exportExcel(res, list, '${context.functionName}数据', ${context.ClassName});
     }
 
     /**
-     * 获取参数配置表详细信息
+     * 获取${context.functionName}详细信息
      */
-    @ApiOperation({ summary: '获取参数配置表详细信息' })
+    @ApiOperation({ summary: '获取${context.functionName}详细信息' })
     @ApiResponse({ status: 200, description: '获取成功', type: ${context.ClassName} })
     @PreAuthorize('${context.permissionPrefix}:query')
     @Get(':${context.pkColumn.tsField}')
-    @Log({ title: '获取参数配置表详细信息' })
+    @Log({ title: '获取${context.functionName}详细信息' })
     async getInfo(@Param('${context.pkColumn.tsField}') ${context.pkColumn.tsField}: number) {
         return this.success(await this.${ClassNameWithoutSysPrefixAndLowerCaseFirstLetter}Service.select${ClassNameWithoutSysPrefix}ById(${context.pkColumn.tsField}));
     }
 
     /**
-     * 新增参数配置表
+     * 新增${context.functionName}
      */
-    @ApiOperation({ summary: '新增参数配置表' })
+    @ApiOperation({ summary: '新增${context.functionName}' })
     @ApiResponse({ status: 201, description: '新增成功' })
     @PreAuthorize('${context.permissionPrefix}:add')
     @Post()
-    @Log({ title: '新增参数配置表', businessType: BusinessType.INSERT })
+    @Log({ title: '新增${context.functionName}', businessType: BusinessType.INSERT })
     async add(@Body() ${context.className}: ${context.ClassName}, @Req() req) {
         const loginUser = req.user;
         ${context.className}.createBy = loginUser.getUsername();
@@ -101,13 +101,13 @@ export class ${context.ClassName}Controller extends BaseController {
     }
 
     /**
-     * 修改参数配置表
+     * 修改${context.functionName}
      */
-    @ApiOperation({ summary: '修改参数配置表' })
+    @ApiOperation({ summary: '修改${context.functionName}' })
     @ApiResponse({ status: 200, description: '修改成功' })
     @PreAuthorize('${context.permissionPrefix}:edit')
     @Put()
-    @Log({ title: '修改参数配置表', businessType: BusinessType.UPDATE })
+    @Log({ title: '修改${context.functionName}', businessType: BusinessType.UPDATE })
     async edit(@Body() ${context.className}: ${context.ClassName}, @Req() req) {
         const loginUser = req.user;
         ${context.className}.updateBy = loginUser.getUsername();
@@ -116,13 +116,13 @@ export class ${context.ClassName}Controller extends BaseController {
     }
 
     /**
-     * 删除参数配置表
+     * 删除${context.functionName}
      */
-    @ApiOperation({ summary: '删除参数配置表' })
+    @ApiOperation({ summary: '删除${context.functionName}' })
     @ApiResponse({ status: 200, description: '删除成功' })
     @PreAuthorize('${context.permissionPrefix}:remove')
     @Delete(':${context.pkColumn.tsField}s')
-    @Log({ title: '删除参数配置表', businessType: BusinessType.DELETE })
+    @Log({ title: '删除${context.functionName}', businessType: BusinessType.DELETE })
     async remove(@Param('${context.pkColumn.tsField}s') ${context.pkColumn.tsField}s: string) {
         const ids = ${context.pkColumn.tsField}s.split(',').map(id => parseInt(id));
         await this.${ClassNameWithoutSysPrefixAndLowerCaseFirstLetter}Service.delete${ClassNameWithoutSysPrefix}ByIds(ids)
